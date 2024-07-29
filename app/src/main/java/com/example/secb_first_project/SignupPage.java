716ff8f666310ccbbd82c1bd1279751d9ce2ac4b
Rahul Kumar <rahul@gmail.com>
2024-07-29 08:21:04 +0545

    Click

--- app/src/main/java/com/example/secb_first_project/SignupPage.java
+++ app/src/main/java/com/example/secb_first_project/SignupPage.java
@@ -1,6 +1,10 @@
 package com.example.secb_first_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignupPage extends AppCompatActivity {
 
+
+    Button btn1;
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn1=findViewById(R.id.btn_signup);
+        txt1=findViewById(R.id.txt_signin);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent btn1Intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(btn1Intent);
+            }
+        });
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent txt1Intent=new Intent(getApplicationContext(), LoginPage.class);
+                startActivity(txt1Intent);
+            }
         });
     }
 }
